52099f370d1f3f9e807f67a657269dfb47e0f6e6
Joshua.C.Lin <joshuaclin77@gmail.com>
2024-06-27 01:35:27 +0800

    Ch2-39

--- first-angular-app/src/app/header.component.ts
+++
@@ -1,6 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: 'app-header'
-})
-export class HeaderComponent {}
---
+++ first-angular-app/src/app/user/user.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input, computed, input, Output, EventEmitter, output } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  @Input({ required: true }) user!: {
+    id: string;
+    avatar: string;
+    name: string;
+  };
+  @Output() select = new EventEmitter<string>();  // select = output<string>();
+
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar();
+  // })
+
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+
+  onSelectUserFn() {
+    this.select.emit(this.user.id);
+  }
+}
