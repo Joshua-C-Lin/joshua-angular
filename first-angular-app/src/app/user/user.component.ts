80985e829b311c7e750f30b80474fe61719036a9
Joshua.C.Lin <joshuaclin77@gmail.com>
2024-06-27 22:37:48 +0800

    Ch2-41

--- first-angular-app/src/app/user/user.component.ts
+++ first-angular-app/src/app/user/user.component.ts
@@ -1,5 +1,11 @@
 import { Component, Input, computed, input, Output, EventEmitter, output } from '@angular/core';
 
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -8,11 +14,7 @@ import { Component, Input, computed, input, Output, EventEmitter, output } from 
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) user!: {
-    id: string;
-    avatar: string;
-    name: string;
-  };
+  @Input({ required: true }) user!: User;
   @Output() select = new EventEmitter<string>();  // select = output<string>();
 
   // imagePath = computed(() => {
